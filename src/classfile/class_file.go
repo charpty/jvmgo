839588155c179d160f5b031a5ece9ab40bcaaa3d
charpty <charpty@gmail.com>
2018-02-26 22:39:20 +0800

    增加：了解常量池结构并梳理读取主逻辑

--- src/classfile/class_file.go
+++ src/classfile/class_file.go
@@ -26,11 +26,11 @@ type ClassFile struct {
 	//magic      uint32
 	minorVersion uint16
 	majorVersion uint16
-	// constantPool ConstantPool
-	accessFlags uint16
-	thisClass   uint16
-	superClass  uint16
-	interfaces  []uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
 	// fields       []*MemberInfo
 	// methods      []*MemberInfo
 	// attributes   []AttributeInfo
@@ -58,7 +58,7 @@ func Parse(classData []byte) (cf *ClassFile, err error) {
 func (self *ClassFile) read(reader *ClassReader) {
 	self.readAndCheckMagic(reader)
 	self.readAndCheckVersion(reader)
-	// self.constantPool = readConstantPool(reader)
+	self.constantPool = readConstantPool(reader)
 	self.accessFlags = reader.readUint16()
 	self.thisClass = reader.readUint16()
 	self.superClass = reader.readUint16()
---
+++ src/classfile/constant_pool.go
@@ -0,0 +1,54 @@
+package classfile
+
+import "fmt"
+
+type ConstantPool []ConstantInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+	// 常量池第一个short存储了常量池元素的个数
+	// 每一个常量都有一个byte标志位和具体的数据组成
+	cpCount := int(reader.readUint16())
+	// 创建一个动态数组，因为有特殊情况
+	cp := make([]ConstantInfo, cpCount)
+
+	// The constant_pool table is indexed from 1 to constant_pool_count - 1.
+	for i := 1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		// http://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.4.5
+		// 这就是个数的特殊情况，读到long和double时，必须下一个元素是个空，以兼容老版本
+		// 这是由于一个byte占常量池2个位置
+		switch cp[i].(type) {
+		//case *ConstantLongInfo, *ConstantDoubleInfo:
+		//i++
+		}
+	}
+
+	return cp
+}
+
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic(fmt.Errorf("Invalid constant pool index: %v!", index))
+}
+
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	//ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	//name := self.getUtf8(ntInfo.nameIndex)
+	//_type := self.getUtf8(ntInfo.descriptorIndex)
+	//return name, _type
+	return "", ""
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	//classInfo := self.getConstantInfo(index).(*ConstantClassInfo)
+	//return self.getUtf8(classInfo.nameIndex)
+	return ""
+}
+
+func (self ConstantPool) getUtf8(index uint16) string {
+	//utf8Info := self.getConstantInfo(index).(*ConstantUtf8Info)
+	//return utf8Info.str
+	return ""
+}
